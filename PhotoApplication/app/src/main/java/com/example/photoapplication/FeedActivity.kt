301ded2d5f6507b943613fd51b4ee02e31d3ac5b
sevgireyhani <60885207+sevgireyhani@users.noreply.github.com>
2021-07-03 15:55:58 +0300

    Photo Application
    a mobile application where users can share photos after registration

---
+++ PhotoApplication/app/src/main/java/com/example/photoapplication/FeedActivity.kt
@@ -0,0 +1,110 @@
+package com.example.photoapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.Timestamp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import kotlinx.android.synthetic.main.activity_feed.*
+
+class FeedActivity : AppCompatActivity() {
+    private  lateinit var  auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
+
+    var userEmailFromFB : ArrayList<String> = ArrayList()
+    var userCommentFB : ArrayList<String> = ArrayList()
+    var userImageFromFB : ArrayList<String> = ArrayList()
+
+    var adapter : FeedRecyclerAdapter? = null
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.options_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.add_post){
+            //Go to Upload Activity
+            val intent = Intent(applicationContext,UploadActivity::class.java)
+            startActivity(intent)
+
+
+
+        }else if(item.itemId == R.id.logout){
+            // Go to Logout
+
+            auth.signOut()
+            val intent = Intent(applicationContext,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feed)
+
+        auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
+
+        getDataFromFirestore()
+
+        //recyclerview
+        var layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+
+        adapter = FeedRecyclerAdapter(userEmailFromFB,userImageFromFB,userImageFromFB)
+        recyclerView.adapter = adapter
+
+
+
+
+    }
+    fun getDataFromFirestore() {
+
+        db.collection( "Posts").orderBy("date",Query.Direction.DESCENDING).addSnapshotListener { snapshot, exception ->
+            if (exception != null) {
+                Toast.makeText(applicationContext,exception.localizedMessage.toString(),Toast.LENGTH_LONG).show()
+            }else{
+                if (snapshot != null){
+                    if(!snapshot.isEmpty){
+
+                        userImageFromFB.clear()
+                        userCommentFB.clear()
+                        userEmailFromFB.clear()
+
+                        val documents = snapshot.documents
+                        for (document in documents) {
+                            val comment = document.get("comment") as String
+                            val useremail = document.get("userEmail") as String
+                            val downloadUrl = document.get("downloadUrl") as String
+                            val timestamp = document.get("date") as Timestamp
+                            val date = timestamp.toDate()
+
+                            userEmailFromFB.add(useremail)
+                            userCommentFB.add(comment)
+                            userImageFromFB.add(downloadUrl)
+
+                            adapter!!.notifyDataSetChanged()
+
+
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+
+
+}
